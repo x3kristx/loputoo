0726d9fdc694b9ce39016c6388c023606bd59788
x3kristx <kristo.lember2@gmail.com>
2025-09-20 17:20:21 +0300

    added background image (placeholder for now) + gradient +new font

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
-    t.window.title = "Game idk"
+    t.window.title = "Freaky Gradient Text"
     t.window.width = 1024
     t.window.height = 768
 end
--- main.lua
+++ main.lua
@@ -1,20 +1,50 @@
 function love.load()
-    bigFont = love.graphics.newFont(48)
-    mediumFont = love.graphics.newFont(32)
-    smallFont = love.graphics.newFont(16)
+    bigFont = love.graphics.newFont("assets/Jersey25-regular.ttf", 48)
+    mediumFont = love.graphics.newFont("assets/Jersey25-regular.ttf", 32)
+    smallFont = love.graphics.newFont"assets/Jersey25-regular.ttf", (16)
+
+    gradientShader = love.graphics.newShader[[
+        extern vec4 colorTop;
+        extern vec4 colorBottom;
+        vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
+        {
+            float t = texture_coords.y;
+            vec4 gradient = mix(colorTop, colorBottom, t);
+            vec4 texColor = Texel(texture, texture_coords); // Sample the texture
+            return texColor * gradient;
+        }
+    ]]
+    backgroundImage = love.graphics.newImage("assets/pardik.jpg")
 end
 function love.draw()
-    local text = "Title Screen"
+    local windowWidth = love.graphics.getWidth()
+    local windowHeight = love.graphics.getHeight()
+
+    -- Draw and stretch the background image
+    local imgWidth = backgroundImage:getWidth()
+    local imgHeight = backgroundImage:getHeight()
+    local scaleX = windowWidth / imgWidth
+    local scaleY = windowHeight / imgHeight
+    love.graphics.draw(backgroundImage, 0, 0, 0, scaleX, scaleY)
+
+    -- Gradient text rendering (same as before)
+    local text = "game title"
     local font = bigFont
     love.graphics.setFont(font)
     local textWidth = font:getWidth(text)
     local textHeight = font:getHeight()
 
-    local windowWidth = love.graphics.getWidth()
-    local windowHeight = love.graphics.getHeight()
+    local canvas = love.graphics.newCanvas(textWidth, textHeight)
+    love.graphics.setCanvas(canvas)
+    love.graphics.clear(0, 0, 0, 0)
+    love.graphics.setFont(font)
+    love.graphics.setColor(1,1,1,1)
+    love.graphics.print(text, 0, 0)
+    love.graphics.setCanvas()
 
-    love.graphics.print(text,
-        (windowWidth - textWidth) / 2, 
-        (windowHeight - textHeight) / 2
-    )
+    gradientShader:send("colorTop", {1, 0, 0, 1})
+    gradientShader:send("colorBottom", {0, 0, 1, 1})
+    love.graphics.setShader(gradientShader)
+    love.graphics.draw(canvas, (windowWidth - textWidth) / 2, (windowHeight - textHeight) / 2)
+    love.graphics.setShader()
 end
