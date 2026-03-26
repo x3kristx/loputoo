6910bc6bde3564d4b6f8da34c889142a73212bc2
x3kristx <kristo.lember2@gmail.com>
2026-03-26 12:43:48 +0200

    Added more weapons to the list

--- weaponlist.lua
+++ weaponlist.lua
@@ -39,3 +39,70 @@ weapon.bow = {
         { name = "Evasive Roll", evasion = function() print("You roll to evade the attack!") end },
     }
 }
+
+weapon.grimoire = {
+    name = "Grimoire"
+    damage = function()
+        return math.random(4, 12)
+    end,
+    skills = {
+        { name = "Fireball", damage = function() return math.random(10, 25) end },
+        { name = "Arcane Shield", block = function() print("You conjure a magical shield to block the attack!") end },
+    }
+}
+
+weapon.chakrams = {
+    name = "Chakrams",
+    damage = function()
+        return math.random(2, 6)
+    end,
+    skills = {
+        { name = "Whirling Blades", damage = function() return math.random(6, 12) end },
+        { name = "Deflect", block = function() print("You deflect the attack with your chakrams!") end },
+    }
+}
+
+weapon.knuckles = {
+    name = "Knuckles",
+    damage = function()
+        return math.random(1, 4)
+    end,
+    skills = {
+        { name = "Rapid Punches", damage = function() return math.random(4, 10) end },
+        { name = "Counter Strike", block = function() print("You counter the attack with a quick punch!") end },
+    }
+}
+
+weapon.halberd = {
+    name = "Halberd",
+    damage = function()
+        return math.random(6, 12)
+    end,
+    skills = {
+        { name = "Sweeping Strike", damage = function() return math.random(12, 20) end },
+        { name = "Guard", block = function() print("You block the enemy's attack with your halberd!") end },
+    }
+}
+
+weapon.greatsword = {
+    name = "Greatsword",
+    damage = function()
+        return math.random(8, 15)
+    end,
+    skills = {
+        { name = "Heavy Slash", damage = function() return math.random(15, 30) end },
+        { name = "Parry", block = function() print("You parry the attack with your greatsword!") end },
+    }
+}
+
+weapon.rapier = {
+    name = "Rapier",
+    damage = function()
+        return math.random(3, 7)
+    end,
+    skills = {
+        { name = "Lunge", damage = function() return math.random(7, 14) end },
+        { name = "Riposte", block = function() print("You riposte the attack with your rapier!") end },
+    }
+}
+
